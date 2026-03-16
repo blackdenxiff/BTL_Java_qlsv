62fc91ae5c454a0f54b08f8871a1c919620ca13f
blackdenxif <dn555178@gmail.com>
2026-03-16 12:02:23 +0700

    DEMO3

--- src/database/Main.java
+++ src/database/Main.java
@@ -12,32 +12,7 @@ import static database.KNDatabase.ketNoiDatabase;
 
 public class Main {
     // Xóa bỏ <SinhVienDAO> ở đây
-    public static void main(String[] args) {
-        
-        // Kiểm tra kết nối thử (tùy vào logic hàm của bạn)
-        try {
+    public static void main(String[] args) throws SQLServerException {
             ketNoiDatabase();
-        } catch (SQLServerException e) {
-            throw new RuntimeException(e);
-        }
-
-        // 1. Tạo đối tượng sinh viên mới
-        SinhVien sv = new SinhVien();
-        sv.setMaSV("SV001");
-        sv.setHoDem("Nguyễn Văn");
-        sv.setTen("An");
-        sv.setNgaySinh(LocalDate.of(2005, 5, 20));
-        sv.setGioiTinh("Nam");
-        sv.setNamNhapHoc(2023);
-        sv.setMaLop("K68-CNTT"); 
-        sv.setSdt("0912345678");
-        sv.setEmail("an.nv@gmail.com");
-        sv.setTrangThaiSV("Đang học");
-        sv.setMaHuyen("H001"); 
-
-        // 2. Gọi DAO để lưu vào SQL Server
-        // Lúc này Java sẽ hiểu SinhVienDAO là lớp bạn đã định nghĩa
-        SinhVienDAO dao = new SinhVienDAO();
-        dao.insertSinhVien(sv);
     }
 }
